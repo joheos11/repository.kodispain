e4a4bf9b45f51387d1e7fc85ff392f605e91897c
joheos11 <konectas@gmail.com>
2017-01-22 14:26:41 +0100

    Repositorio KodiSpain
    Primera version del repositorio de kodispain. Puedes seguirme en
    http://t.me/kodispain

---
+++ plugin.video.kodispainbuild/default.py
@@ -0,0 +1,232 @@
+import xbmc, xbmcaddon, xbmcgui, xbmcplugin,os,sys
+import shutil
+import urllib2,urllib
+import re
+import extract
+import time
+import downloader
+import plugintools
+import zipfile
+import ntpath
+
+
+USER_AGENT = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'
+base='http://konectas.es'
+ADDON=xbmcaddon.Addon(id='plugin.video.kodispainbuild')
+dialog = xbmcgui.Dialog()    
+VERSION = "1.0"
+PATH = "kodispainbuild"            
+
+    
+def CATEGORIES():
+    link = OPEN_URL('https://www.dropbox.com/s/me1vsq7guj4i6f5/wizard.txt?dl=1').replace('\n','').replace('\r','')
+    match = re.compile('name="(.+?)".+?rl="(.+?)".+?mg="(.+?)".+?anart="(.+?)".+?escription="(.+?)"').findall(link)
+    for name,url,iconimage,fanart,description in match:
+        addDir(name,url,1,iconimage,fanart,description)
+    setView('movies', 'MAIN')
+        
+    
+def OPEN_URL(url):
+    req = urllib2.Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+    response = urllib2.urlopen(req)
+    link=response.read()
+    response.close()
+    return link
+    
+    
+def wizard(name,url,description):
+    path = xbmc.translatePath(os.path.join('special://home/addons','packages'))
+    dp = xbmcgui.DialogProgress()
+    dp.create("KodiSpain","Descargando ",'', 'Por favor, espere')
+    lib=os.path.join(path, name+'.zip')
+    try:
+       os.remove(lib)
+    except:
+       pass
+    downloader.download(url, lib, dp)
+    addonfolder = xbmc.translatePath(os.path.join('special://','home'))
+    time.sleep(2)
+    dp.update(0,"", "Extrayendo Zip")
+    print '======================================='
+    print addonfolder
+    print '======================================='
+    extract.all(lib,addonfolder,dp)
+    dialog = xbmcgui.Dialog()
+    dialog.ok("DESCARGA COMPLETA", 'Desafortunadamente, la única manera de conseguir los nuevos cambios', 'es forzar el cierre de Kodi. Haga clic en Aceptar para forzar a Kodi a cerrar,', 'NO use las opciones normales de Salir de Kodi. Si el cierre forzado no funciona por alguna razón, por favor Reinicie el Dispositivo o finalice la tarea manualmente')
+    killxbmc()
+        
+      
+        
+def killxbmc():
+    choice = xbmcgui.Dialog().yesno('Fuerza Cerrar Kodi', 'Estás a punto de cerrar Kodi', '¿Te gustaría continuar?', nolabel='No, Cancelar',yeslabel='Sí, Cerrar')
+    if choice == 0:
+        return
+    elif choice == 1:
+        pass
+    myplatform = platform()
+    print "Platform: " + str(myplatform)
+    if myplatform == 'osx': # OSX
+        print "############   try osx force close  #################"
+        try: os.system('killall -9 XBMC')
+        except: pass
+        try: os.system('killall -9 Kodi')
+        except: pass
+        dialog.ok("[COLOR=red][B]Atención  !!![/COLOR][/B]", "Si estás viendo este mensaje es porque el cierre forzado", "ha fallado. Por favor, fuerza el cierre de Kodi [COLOR=lime]NO USE[/COLOR] el menu para Salir.",'')
+    elif myplatform == 'linux': #Linux
+        print "############   try linux force close  #################"
+        try: os.system('killall XBMC')
+        except: pass
+        try: os.system('killall Kodi')
+        except: pass
+        try: os.system('killall -9 xbmc.bin')
+        except: pass
+        try: os.system('killall -9 kodi.bin')
+        except: pass
+        dialog.ok("[COLOR=red][B]Atención  !!![/COLOR][/B]", "Si estás viendo este mensaje es porque el cierre forzado", "ha fallado. Por favor, fuerza el cierre de Kodi [COLOR=lime]NO USE[/COLOR] el menu para Salir.",'')
+    elif myplatform == 'android': # Android  
+        print "############   try android force close  #################"
+        try: os.system('adb shell am force-stop org.xbmc.kodi')
+        except: pass
+        try: os.system('adb shell am force-stop org.kodi')
+        except: pass
+        try: os.system('adb shell am force-stop org.xbmc.xbmc')
+        except: pass
+        try: os.system('adb shell am force-stop org.xbmc')
+        except: pass        
+        dialog.ok("[COLOR=red][B]Atención  !!![/COLOR][/B]", "Tu sistema ha sido detectado como Android, por tanto ", "[COLOR=yellow][B]DEBES[/COLOR][/B] forzar el cierre de Kodi. [COLOR=lime]NO USE[/COLOR] el menu para Salir.","Cierre con el Administrador de tareas (si no está seguro, desenchufe su dispositivo).")
+    elif myplatform == 'windows': # Windows
+        print "############   try windows force close  #################"
+        try:
+            os.system('@ECHO off')
+            os.system('tskill XBMC.exe')
+        except: pass
+        try:
+            os.system('@ECHO off')
+            os.system('tskill Kodi.exe')
+        except: pass
+        try:
+            os.system('@ECHO off')
+            os.system('TASKKILL /im Kodi.exe /f')
+        except: pass
+        try:
+            os.system('@ECHO off')
+            os.system('TASKKILL /im XBMC.exe /f')
+        except: pass
+        dialog.ok("[COLOR=red][B]Atención  !!![/COLOR][/B]", "Si estás viendo este mensaje es porque el cierre forzado", "ha fallado. Por favor, fuerza el cierre de Kodi [COLOR=lime]NO USE[/COLOR] el menu para Salir.","Use el gestor de tareas y NO ALT F4")
+    else: #ATV
+        print "############   try atv force close  #################"
+        try: os.system('killall AppleTV')
+        except: pass
+        print "############   try raspbmc force close  #################" #OSMC / Raspbmc
+        try: os.system('sudo initctl stop kodi')
+        except: pass
+        try: os.system('sudo initctl stop xbmc')
+        except: pass
+        dialog.ok("[COLOR=red][B]Atención  !!![/COLOR][/B]", "Si estás viendo este mensaje es porque el cierre forzado", "ha fallado. Por favor, fuerza el cierre de Kodi [COLOR=lime]NO USE[/COLOR] el menu para Salir.","No se pudo detectar su Sistema, así que desenchufe su dispositivo.")    
+
+def platform():
+    if xbmc.getCondVisibility('system.platform.android'):
+        return 'android'
+    elif xbmc.getCondVisibility('system.platform.linux'):
+        return 'linux'
+    elif xbmc.getCondVisibility('system.platform.windows'):
+        return 'windows'
+    elif xbmc.getCondVisibility('system.platform.osx'):
+        return 'osx'
+    elif xbmc.getCondVisibility('system.platform.atv2'):
+        return 'atv2'
+    elif xbmc.getCondVisibility('system.platform.ios'):
+        return 'ios'
+
+
+def addDir(name,url,mode,iconimage,fanart,description):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&iconimage="+urllib.quote_plus(iconimage)+"&fanart="+urllib.quote_plus(fanart)+"&description="+urllib.quote_plus(description)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name, "Plot": description } )
+        liz.setProperty( "Fanart_Image", fanart )
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
+        return ok
+        
+       
+        
+def get_params():
+        param=[]
+        paramstring=sys.argv[2]
+        if len(paramstring)>=2:
+                params=sys.argv[2]
+                cleanedparams=params.replace('?','')
+                if (params[len(params)-1]=='/'):
+                        params=params[0:len(params)-2]
+                pairsofparams=cleanedparams.split('&')
+                param={}
+                for i in range(len(pairsofparams)):
+                        splitparams={}
+                        splitparams=pairsofparams[i].split('=')
+                        if (len(splitparams))==2:
+                                param[splitparams[0]]=splitparams[1]
+                                
+        return param
+        
+                      
+params=get_params()
+url=None
+name=None
+mode=None
+iconimage=None
+fanart=None
+description=None
+
+
+try:
+        url=urllib.unquote_plus(params["url"])
+except:
+        pass
+try:
+        name=urllib.unquote_plus(params["name"])
+except:
+        pass
+try:
+        iconimage=urllib.unquote_plus(params["iconimage"])
+except:
+        pass
+try:        
+        mode=int(params["mode"])
+except:
+        pass
+try:        
+        fanart=urllib.unquote_plus(params["fanart"])
+except:
+        pass
+try:        
+        description=urllib.unquote_plus(params["description"])
+except:
+        pass
+        
+        
+print str(PATH)+': '+str(VERSION)
+print "Mode: "+str(mode)
+print "URL: "+str(url)
+print "Name: "+str(name)
+print "IconImage: "+str(iconimage)
+
+
+def setView(content, viewType):
+    # set content type so library shows more views and info
+    if content:
+        xbmcplugin.setContent(int(sys.argv[1]), content)
+    if ADDON.getSetting('auto-view')=='true':
+        xbmc.executebuiltin("Container.SetViewMode(%s)" % ADDON.getSetting(viewType) )
+        
+        
+if mode==None or url==None or len(url)<1:
+        CATEGORIES()
+       
+elif mode==1:
+        wizard(name,url,description)
+        
+
+        
+xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
